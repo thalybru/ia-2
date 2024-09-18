d64da86b71cb94beb6a757b633a85b7febfe763c
thalybru <106528975+thalybru@users.noreply.github.com>
2024-09-18 08:58:26 -0300

    Update aleatorio.js

--- ia-2-main/aleatorio.js
+++ ia-2-main/aleatorio.js
@@ -1,2 +1,8 @@
-const nome [Tomas,Isaque, Eliza, Sara, Diana, Nick, Bob
-            ]
+const nome [Tomas,Isaque, Eliza, Sara, Diana, Nick, Bob  ]
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
